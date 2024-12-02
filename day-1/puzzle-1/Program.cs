5221b48aa36f430c8884df135c494c70e1886ce7
TrevorLinkgreen <trevor.watson@linkgreen.ca>
2024-12-02 08:49:35 -0500

    day 1 puzzle 1

---
+++ day-1/puzzle-1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+Console.WriteLine("Hello, World!");
+
+var lines = File.ReadAllLines("input.txt").ToList();
+
+var (list1, list2) = (
+    lines.Select(l => int.Parse(l.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0].Trim())).OrderBy(l => l).ToList(),
+    lines.Select(l => int.Parse(l.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1].Trim())).OrderBy(l => l).ToList()
+);
+
+var distance = list1.Select((l1, idx) =>
+{
+    return Math.Abs(l1 - list2.ElementAt(idx));
+});
+
+Console.WriteLine(distance.Sum());
